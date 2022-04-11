0e2ef5ddfa714b62a69e9956404acf466262d759
Simon Petrac <pfoerdie@gmail.com>
2022-04-11 15:37:55 +0200

    developing tests

--- test/agent.server.test.js
+++ test/agent.server.test.js
@@ -12,7 +12,7 @@ describe('agent.server', function () {
 
     let context, factory, store, space;
 
-    before('setup space', async function () {
+    before('setup space etc.', async function () {
         context = {
             // public ontologies
             'rdf':  'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
@@ -56,8 +56,8 @@ describe('agent.server', function () {
             agent = await ServerAgent.create({
                 port:   8192,
                 store:  store,
-                app:    true,
-                server: true
+                server: true,
+                app:    true
             });
             await agent.listen();
             expect(agent.server.listening).toBeTruthy();
@@ -67,10 +67,57 @@ describe('agent.server', function () {
         test('add "/hello" route and get a response', async function () {
             agent.app.get('/hello', (request, response) => response.send('Hello World!'));
 
-            const response = await fetch(agent.url + 'hello');
+            const response = await fetch('http://localhost:8192/hello');
             expect(response.ok).toBeTruthy();
             expect(await response.text()).toBe('Hello World!');
         });
+
+        // TODO more tests
+
+        after('close agent', async function () {
+            await agent.close();
+            expect(agent.server.listening).toBeFalsy();
+        });
+
+    });
+
+    describe('usage as http and socket io server with domain and amec', function () {
+
+        let agent;
+
+        before('setup agent', async function () {
+            agent = await ServerAgent.create({
+                port:     8192,
+                space:    space,
+                server:   true,
+                sessions: true,
+                app:      true,
+                io:       true,
+                domain:   true,
+                amec:     true
+            });
+            await agent.listen();
+            expect(agent.server.listening).toBeTruthy();
+            expect(agent.url).toBe('http://localhost:8192/');
+        });
+
+        test('DEVELOP', async function () {
+            console.log({
+                agent:    agent,
+                url:      agent.url,
+                uri:      agent.uri,
+                node:     agent.node,
+                space:    agent.space,
+                server:   agent.server,
+                app:      agent.app,
+                io:       agent.io,
+                sessions: agent.sessions,
+                domain:   agent.domain,
+                amec:     agent.amec
+            });
+        });
+
+        // TODO more tests
 
         after('close agent', async function () {
             await agent.close();
